63c0d1c96a0f548c144939c8a8fea5bbac9cb3de
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2024-08-13 21:05:48 -0300

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+package repositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfiguration {
+
+    @Bean
+    public void runEveryTime() {
+        // Código para ser executado toda vez que o projeto é executado no modo de teste
+    }
+}
